805ef19f49fb4bfc3350a6814cace2dec823aae7
Soni Buda <181140941+sonibuda@users.noreply.github.com>
2024-12-22 12:32:02 -0700

    Write a function to pass the first 6 tests.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return "ERROR";
+  } 
+  
 
-};
+  let repeated = "";
+  for (let i = 0; i < num; i++) {
+   
+    repeated += string;
+  }
 
+  return repeated;
+}
+
+const repeatedStringResult = repeatString("bye",-1);
+console.log(repeatedStringResult);
 // Do not edit below this line
 module.exports = repeatString;
