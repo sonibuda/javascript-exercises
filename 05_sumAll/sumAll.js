a648e3a41fb2ffdc4b5921734765cf8afa83b6ed
Soni Buda <181140941+sonibuda@users.noreply.github.com>
2024-12-24 10:10:28 -0700

    Write a function to sum given integers.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b)) {
+        return "ERROR";
+    }
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        return "ERROR";
+    }
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+ const n = Math.abs(b - a) + 1;
+ const min = Math.min(a, b);
+ const max = Math.max(a, b);
+ 
+ return n * (min + max) / 2;
+}
 
-};
+
 
 // Do not edit below this line
 module.exports = sumAll;
+
