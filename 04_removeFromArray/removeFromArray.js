405feb6406eae938414794b27803253a6f9b7b04
Soni Buda <181140941+sonibuda@users.noreply.github.com>
2024-12-23 17:09:22 -0700

    Write a function that removes elements from an array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...toBeRemoved) {
+    return array.filter(element => !toBeRemoved.includes(element));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
