642d03490b93d55891bb52d2448d8dff808186ff
Soni Buda <181140941+sonibuda@users.noreply.github.com>
2024-12-25 12:42:27 -0700

    Write a series of functions to calculate sums, differences, exponents, products, and factorials.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
+const add = function(num1, num2) {
+  return num1 + num2;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  return num1 - num2;
 	
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
 	
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((accumulator, currentValue) => accumulator * currentValue);
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0 || number === 1) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 2; i <= number; i++) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
