e0eb63104c6140718c5cabe6a8530bea4ce4d6a3
Soni Buda <181140941+sonibuda@users.noreply.github.com>
2024-12-26 13:26:03 -0700

    Write a function to create an array of book titles from an existing array of authors and titles.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = books.map(book => {
+        return book.title;
+    });
+    return titles;
 };
+  
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
