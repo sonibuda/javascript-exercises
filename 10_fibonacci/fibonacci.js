548581a1e576b7fff815eb5b4277c3fa61087e98
Soni Buda <181140941+sonibuda@users.noreply.github.com>
2024-12-26 08:56:39 -0700

    Write a function to return the correct number in a Fibonacci sequence.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(itemInSequence) {
+   const fibSequence = [0, 1];
 
+   for(let i = 2; i <= itemInSequence; i++) {
+    fibSequence[i] = fibSequence[i - 1] + fibSequence[i -2];
+   }
+
+   if (itemInSequence < 0) {
+    return "OOPS";
+   } else {
+    return fibSequence[itemInSequence];
+}
 };
 
 // Do not edit below this line
