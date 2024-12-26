48c8854c3c30228f1d7c347de9c92d6460883cb2
Soni Buda <181140941+sonibuda@users.noreply.github.com>
2024-12-25 19:27:05 -0700

    Write a function to return true if palindrome else false.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (...phrase) {
+const joinedPhrase = phrase
+  .join('')
+  .toLowerCase()
+  .replace(/[^a-z0-9]/g, '');
 
-};
+const reversedPhrase = joinedPhrase
+  .split('')
+  .reverse()
+  .join('');
+
+  return reversedPhrase === joinedPhrase;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
